8f99e9eea0b8d62351fc12113ba8c6769c8be2fb
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-02-24 16:05:12 -0300

    feat: adding initial styles

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,6 @@
 import { Text, View } from "react-native";
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 
 export function Groups() {
-  return (
-    <Container>
-      <Title>Groups</Title>
-    </Container>
-  );
+  return <Container></Container>;
 }
