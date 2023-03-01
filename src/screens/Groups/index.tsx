279c4792862218bc22882fe4147c4d203f5f6ff7
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-03-01 14:41:36 -0300

    fix: adjusting group card screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,42 @@
+import { Button } from "@components/Button";
+import { EmptyList } from "@components/EmptyList";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
+interface GroupsData {
+  id: string;
+  name: string;
+}
+
 export function Groups() {
+  const [groups, setGroups] = useState<GroupsData[]>([]);
+
+  function generateId() {
+    return Math.floor(Math.random() + new Date().getTime()).toString();
+  }
+
   return (
     <Container>
-      <Header showBackButton />
+      <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Test" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <GroupCard title={item.name} onPress={() => {}} />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={
+          <EmptyList message="Que tal cadastrar a primeira turma?" />
+        }
+      />
+
+      <Button text="Criar nova turma" />
     </Container>
   );
 }
