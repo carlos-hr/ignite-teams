6092567bbf2f3cc7abd603884e6cb12c94e7fb70
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-03-02 10:38:20 -0300

    feat: creating new group screen

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Container, Content, Icon } from "./styles";
+
+export function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Content>
+        <Icon />
+        <Highlight
+          title="Nova turma"
+          subtitle="Crie uma nova turma para adicionar pessoas"
+        />
+
+        <Button text="Criar" />
+      </Content>
+    </Container>
+  );
+}
