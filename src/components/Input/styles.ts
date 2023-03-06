b45fbba7a462f7fbaad726b3c6387bbf8c20c9e9
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-03-06 14:00:06 -0300

    fix: adjusting styles

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,9 +1,7 @@
 import { TextInput } from "react-native";
 import styled from "styled-components/native";
 
-export const Container = styled(TextInput).attrs(({ theme }) => ({
-  placeholderTextColor: theme.COLORS.GRAY_300,
-}))`
+export const Container = styled(TextInput)`
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
   font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
