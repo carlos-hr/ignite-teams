7c4607eb9e718750dcf41929bad9159465233fb3
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-03-07 09:34:39 -0300

    fix: using css helper

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,14 +1,17 @@
 import { TextInput } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TextInput)`
-  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  color: ${({ theme }) => theme.COLORS.WHITE};
   border-radius: 6px;
   min-height: 56px;
   max-height: 56px;
   padding: 16px;
   flex: 1;
+
+  ${({ theme }) => css`
+    background-color: ${theme.COLORS.GRAY_700};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.WHITE};
+  `}
 `;
